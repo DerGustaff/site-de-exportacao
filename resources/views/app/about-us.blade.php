78b5ed70abc5c00c8ecc1e8c893690bfa430f415
DerGustaff <gustavo.souza584@etec.sp.gov.br>
2024-04-30 17:14:42 -0300

    antes de mais nada

---
+++ resources/views/app/about-us.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.app')
+
+@section('title')
+    <title>Sobre nós</title>
+@endsection
+
+@section('css')
+@endsection
+
+@section('body')
+    <div>
+        <p>Está é a página contendo as informações sobre nós.</p>
+    </div>
+@endsection
+
+@section('js')
+    <script>
+        console.log("I am in the contact page.");
+    </script>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,13 @@
 use App\Http\Controllers\ContactController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/contact', function () {
+    return view('app.contact');
+})->name('contact');
+
+Route::get('/about-us', function () {
+    return view('app.about-us');
+})->name('about-us');
 
 
 Route::get('contact', [ContactController::class, 'contact']);
